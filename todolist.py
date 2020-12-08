50ba6082efe5ff6a3d9785be5cec84bd05984e42
Snaiel <christiansaniel611@gmail.com>
2020-12-08 21:32:30 +0800

    Update todolist.py
    Added a menus dict. Added a hierarchy index. Change the right click menu depending on the hierarchy index of element. Cleaned up the creation of elements. Streamlined the opnening and closing of sections. Can now check or uncheck a task by clicking on the text.

--- todolist.py
+++ todolist.py
@@ -3,12 +3,19 @@ import PySimpleGUI as sg
 SYMBOL_RIGHT ='►'
 SYMBOL_DOWN =  '▼'
 
+menus = {
+        'Menu Bar': [['Add', ['Task', 'Section']],['Lists', ['View', 'Add']]],
+        'Task 0 & 1': ['Right', [['Insert', ['Task', 'Section']], 'Rename', 'Delete']],
+        'Section 0 & 1': ['&Right', ['&Add', ['Task', 'Section'], '&Insert', ['Task', 'Section'], 'Rename', 'Delete']],
+        'Task 2': ['Right', [['Insert', ['Task']], 'Rename', 'Delete']],
+        'Section 2': ['&Right', ['&Add', ['Task'], '&Insert', ['Task', 'Section'], 'Rename', 'Delete']]
+        }
+
 menu = [['Add', ['Task', 'Section']],      
         ['Lists', ['View', 'Add']
      ]]
 
-section_right_click = ['&Right', [['&Add', ['Task', 'Section', ]], 'Rename', 'Delete']]
-task_right_click = ['&Right', [['&Rename', 'Delete']]]
+section_right_click = ['&Right', ['&Add', ['Task', 'Section'], '&Insert', ['Task', 'Section'], 'Rename', 'Delete']]
 
 programValues = {'List': 'Project 1', 'ListIndex': '01', 'latestElementRightClicked': ''}
 
@@ -29,8 +36,6 @@ data = [
     ['Today', [{'Daily': True}, {'Cry': False, 'Protein shake': True}], {'Methods homework': False}, {'Physics': True}, [{'Section 1': True}, {'Learn python': False}, {'Buy Furniture': True}], [{'Section 2': False}, {'Workout': False}]],
     ['Project 1', {'Sell stocks': False}, [{'Section 3': False}, {'Lift weights': False}], [{'Tessubcontent': True}, {'Cook': False}, [{'Work': False}, {'Fix bug': False}], {'Feed dog': True}, {'Train dragon': False}]]
 ]
-
-layoutForEachToDoList = {}
 
 def collapse(layout, key, isVisible):
     """
@@ -53,22 +58,41 @@ def createCombo():
         combo.append(i[0])
     combo.append('                         Add List')
 
-def createCheckBox(name, checked, listName):
+def createTask(name, checked, listName, hierarchyIndex):
     for i in data:
         if i[0] == listName:
             ListIndex = str(data.index(i)).zfill(2)
-            checkBoxKey = f"{ListIndex} CHECKBOX {name}"
-            checkBoxTextKey = f"{ListIndex} CHECKBOX TEXT {name}"
-            elementKeys.append(checkBoxTextKey)
-            return [sg.Checkbox('', default=checked, enable_events=True, key=checkBoxKey, pad=((10, 0),(3,3))), sg.T(name, right_click_menu=task_right_click, pad=(0,0), key=checkBoxTextKey)]
+            hierarchyIndex = str(hierarchyIndex).zfill(2)
+            elementIndexes = f"{ListIndex} {hierarchyIndex}"
 
-def createSection(header, opened, content, listName):
+            checkBoxKey = f"{elementIndexes} TASK CHECKBOX {name}"
+            checkBoxTextKey = f"{elementIndexes} TASK TEXT {name}"
+
+            rightClickMenu = menus['Task 0 & 1']
+            if hierarchyIndex == '02':
+                rightClickMenu = menus['Task 2']
+
+            elementKeys.append(f"{elementIndexes} TASK {name}")
+            return [sg.Checkbox('', default=checked, enable_events=True, key=checkBoxKey, pad=((10, 0),(3,3))), sg.T(name, right_click_menu=rightClickMenu, pad=(0,0), key=checkBoxTextKey, enable_events=True)]
+
+def createSection(header, opened, content, listName, hierarchyIndex):
     SectionsOpen[f'{header}'] = opened
     for i in data:
         if i[0] == listName:
             ListIndex = str(data.index(i)).zfill(2)
-            elementKeys.append(f"{ListIndex} {header}")
-            return [[sg.T(symbol(opened), enable_events=True, k=f'{ListIndex} {header} ARROW', pad=((10, 0),(3,3))), sg.T(header, enable_events=True, k=f'{ListIndex} {header}', right_click_menu=section_right_click)], [collapse(content, f'{ListIndex} {header} CONTENT', opened)]]
+            hierarchyIndex = str(hierarchyIndex).zfill(2)
+            elementIndexes = f"{ListIndex} {hierarchyIndex}"
+
+            sectionArrowKey = f'{elementIndexes} SECTION ARROW {header}'
+            sectionTextKey = f'{elementIndexes} SECTION TEXT {header}'
+            sectionContentKey = f'{elementIndexes} SECTION CONTENT {header}'
+
+            rightClickMenu = menus['Section 0 & 1']
+            if hierarchyIndex == '02':
+                rightClickMenu = menus['Section 2']
+
+            elementKeys.append(f"{elementIndexes} SECTION {header}")
+            return [[sg.T(symbol(opened), enable_events=True, k=sectionArrowKey, pad=((10, 0),(3,3))), sg.T(header, enable_events=True, k=sectionTextKey, right_click_menu=rightClickMenu)], [collapse(content, sectionContentKey, opened)]]
 
 def createListLayout(theList):
     createdListLayout = []
@@ -78,7 +102,7 @@ def createListLayout(theList):
             for content in contents:
                 if type(content) is dict:
                     for key, value in content.items():
-                        createdListLayout.append(createCheckBox(key, value, theList))
+                        createdListLayout.append(createTask(key, value, theList, 0))
 
                 if type(content) is list:
                     for key, value in content[0].items():
@@ -88,10 +112,9 @@ def createListLayout(theList):
                     sectionContent = []
 
                     for contentInSection in content:
-
                         if type(contentInSection) is dict and content.index(contentInSection) != 0:
                             for key, value in contentInSection.items():
-                                sectionContent.append(createCheckBox(key, value, theList))
+                                sectionContent.append(createTask(key, value, theList, 1))
 
                         if type(contentInSection) is list:
                             for key, value in contentInSection[0].items():
@@ -103,12 +126,12 @@ def createListLayout(theList):
 
                                 if type(contentInSubSection) is dict and contentInSection.index(contentInSubSection) != 0:
                                     for key, value in contentInSubSection.items():
-                                        subsectionContent.append(createCheckBox(key, value, theList))
+                                        subsectionContent.append(createTask(key, value, theList, 2))
 
-                            for i in createSection(subheader, subopened, subsectionContent, theList):
+                            for i in createSection(subheader, subopened, subsectionContent, theList, 1):
                                 sectionContent.append(i)
 
-                    for i in createSection(header, opened, sectionContent, theList):
+                    for i in createSection(header, opened, sectionContent, theList, 0):
                         createdListLayout.append(i)    
     return createdListLayout
 
@@ -184,13 +207,15 @@ def updateData(dataType, name):
 
 def bindRightClick():
     for i in elementKeys:
-        window[i].bind('<Button-3>', ' +RIGHT CLICK+')
+        elementKey = i.split(' ')
+        elementKey.insert(3, 'TEXT')
+        window[' '.join(elementKey)].bind('<Button-3>', ' +RIGHT CLICK+')
 
 def renameElement(oldKey, newName):
-    if 'CHECKBOX' in oldKey:
-        oldName = oldKey[17:]
+    if 'TASK' in oldKey:
+        oldName = oldKey[16:]
     else:
-        oldName = oldKey[3:]
+        oldName = oldKey[19:]
 
     for i in data:
         if i[0] == programValues['List']:
@@ -217,10 +242,12 @@ def renameElement(oldKey, newName):
             break
 
 def delElement(elementKey):
-    if 'CHECKBOX' in elementKey:
-        elementName = elementKey[17:]
+    if 'TASK' in elementKey:
+        elementName = elementKey[16:]
     else:
-        elementName = elementKey[3:]
+        elementName = elementKey[19:]
+
+    print(elementName)
 
     for i in data:
         if i[0] == programValues['List']:
@@ -262,7 +289,8 @@ def getTxt(msg):
 createCombo()
 
 window = sg.Window('TODOlist', layout=createLayout(None), size=(300,500), finalize=True)
-#print(elementKeys)
+print(elementKeys)
+#print(SectionsOpen)
 
 def createNewWindow():
     global window
@@ -275,7 +303,7 @@ bindRightClick()
 
 while True:             # Event Loop
     event, values = window.read()
-    print(event)
+    #print(event)
 
     if event == sg.WIN_CLOSED or event == 'Exit':
         break
@@ -298,6 +326,7 @@ while True:             # Event Loop
                     # ADDING A TASK OR SECTION
     if event == 'ADDTASK' or event == 'Task':       # Add A Task
         taskToAdd = getTxt('Task Name')
+        #hierarchyIndex = 0
 
         if f"{programValues['ListIndex']} CHECKBOX {taskToAdd}" in values:
             currentLoc = window.CurrentLocation()
@@ -315,29 +344,35 @@ while True:             # Event Loop
             createNewWindow()
                 
 
+    # Opening and closing sections
+    if 'SECTION' in event and 'RIGHT CLICK' not in event:
+        elementIndexes = event[:5]
 
-    # Closing and opening sections
-    if 'ARROW' in event:
-        eventName = event.replace(' ARROW', '')
-        eventName = eventName[3:]
+        if 'ARROW' in event:
+            eventName = event[20:]
+        else:
+            eventName = event[19:]
+
         SectionsOpen[eventName] = not SectionsOpen[eventName]
-        window[f"{event}"].update(SYMBOL_DOWN if SectionsOpen[eventName] else SYMBOL_RIGHT)
-        window[f"{programValues['ListIndex']} {eventName} CONTENT"].update(visible=SectionsOpen[eventName]) 
-        #print(SectionsOpen)
+        window[f"{elementIndexes} SECTION ARROW {eventName}"].update(SYMBOL_DOWN if SectionsOpen[eventName] else SYMBOL_RIGHT)
+        window[f"{elementIndexes} SECTION CONTENT {eventName}"].update(visible=SectionsOpen[eventName]) 
         updateData('section', eventName)
-        #print(data)
-
-    if event in elementKeys:
-        eventName = event[3:]
-        SectionsOpen[eventName] = not SectionsOpen[eventName]
-        window[f"{event} ARROW"].update(SYMBOL_DOWN if SectionsOpen[eventName] else SYMBOL_RIGHT)
-        window[f"{event} CONTENT"].update(visible=SectionsOpen[eventName])
-        updateData('section', event)
 
 
     # Updating the checkbox
-    if 'CHECKBOX' in event:
-        eventName = event.replace(f"{programValues['ListIndex']} CHECKBOX ", '')
+    if 'TASK' in event and 'RIGHT CLICK' not in event:
+
+        if 'TEXT' in event:
+            eventName = event[16:]
+            elementKey = event.split(' ')
+            elementKey.remove('TEXT')
+            elementKey.insert(3, 'CHECKBOX')
+            elementKey = ' '.join(elementKey)
+            checked =  window[elementKey].Get()
+            window[elementKey].Update(value=not checked)
+        else:
+            eventName = event[20:]
+
         updateData('checkbox', eventName)
 
 
@@ -348,7 +383,7 @@ while True:             # Event Loop
 
         if elementKey is not programValues['latestElementRightClicked']:
             programValues['latestElementRightClicked'] = elementKey
-            #print(f"latest element right clicked was: {elementKey}")
+            print(f"Element right clicked was: {elementKey}")
 
         event = window[elementKey].user_bind_event
         window[elementKey]._RightClickMenuCallback(event)
